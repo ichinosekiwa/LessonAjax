a04f7799450c1615b007893bfb4c4b0efaf87eff
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-12-24 10:40:12 +0900

    add:thenを使用したバージョン用にindex02.jsを追加

---
+++ index02.js
@@ -0,0 +1,49 @@
+// https://zipcloud.ibsnet.co.jp/api/search?zipcode=160-0021
+console.log('hello');
+
+document.getElementById('button').addEventListener('click', addressData);
+async function addressData() {
+  const zipcodeItem = document.getElementById('text').value;
+  const searchResult = document.getElementById('address');
+  const errorTxt = document.getElementById('error');
+  const loadingImg = document.getElementById('loadingImg');
+
+  // 通信中のgifを表示
+  loadingImg.style.marginTop = '24px';
+  loadingImg.style.display = 'block';
+
+  // 郵便番号が半角数字7桁であるかをチェック
+  if (!/^\d{7}$/.test(zipcodeItem)) {
+    errorTxt.textContent = '正しく入力してください';
+    searchResult.textContent = '';
+    loadingImg.style.display = 'none';
+    return;
+  }
+
+  //   try {
+  //     const res = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcodeItem}`);
+  //     const data = await res.json();
+
+  fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipcodeItem}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.results && data.results.length > 0) {
+        // 郵便番号検索APIから都道府県・市区町村・町域名を取得
+        const addressItem = data.results[0].address1 + data.results[0].address2 + data.results[0].address3;
+        searchResult.textContent = addressItem;
+        errorTxt.textContent = '';
+      } else {
+        errorTxt.textContent = '住所が見つかりませんでした';
+        searchResult.textContent = '';
+      }
+    })
+    .catch((error) => {
+      console.error('エラーが発生しました:', error);
+      errorTxt.textContent = 'エラーが発生しました。';
+      searchResult.textContent = '';
+    })
+    .finally(() => {
+      // 通信中のgifを削除
+      loadingImg.style.display = 'none';
+    });
+}
